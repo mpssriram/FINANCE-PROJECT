59bbcc7b589d3d0d65c739e33e4c672f11fbf7cd
mpssriram <msriram.0517@gmail.com>
2025-12-28 23:30:23 +0530

    Update web_app.py

--- web_app.py
+++ web_app.py
@@ -66,7 +66,9 @@ def Url_link():
     return URL_Link(datas)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
     
 
     
